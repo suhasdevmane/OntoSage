c1e8011e4099aabc5864cddb16de8571c68227e0
suhas devmane <suhasdevmane@outlook.com>
2025-10-01 02:41:24 +0100

    added graphDB

---
+++ development/graphDB/data/settings.js
@@ -0,0 +1,30 @@
+{
+  "properties" : {
+    "current.location" : ""
+  },
+  "import.local" : {
+    "mythingsboard;;abacws-building-v5.ttl" : {
+      "name" : "abacws-building-v5.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "http://abacwsbuilding.cardiff.ac.uk/abacws#",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "90994dc5-c26f-4581-9148-b13e8b26a149",
+      "timestamp" : 1758040039265,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    }
+  }
+}
